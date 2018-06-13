0659672fc3220680b0d6dc6eb4cf6ecf97cc099a
Cook1619 <cook6348@gmail.com>
2018-06-13 09:25:43 -0500

    Still not quite working

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 slowMath.add(6, 2)
     .then((result) => {
         console.log(result);
-        slowMath.multiply(result, 2)
-            .then((result) => {
-                console.log(result);
-                slowMath.divide(result, 4)
-                    .then((result) => {
-                        console.log(result);
-                        slowMath.subtract(3, result)
-                            .then((result) => {
-                                console.log(result);
-                                slowMath.add(98, result)
-                                    .then((result) => {
-                                        console.log(result);
-                                        slowMath.remainder(result, 2)
-                                            .then((result) => {
-                                                console.log(result);
-                                                slowMath.multiply(result, 50)
-                                                    .then((result) => {
-                                                        console.log(result);
-                                                        slowMath.divide(result, 40)
-                                                            .then((result) => {
-                                                                console.log(result);
-                                                                slowMath.add(32, result)
-                                                                    .then((result) => {
-                                                                        console.log(result);
-                                                                        setTimeout(() => {
-                                                                            console.log(`The final result is ${result}.`)
-                                                                        }, 3000)
-                                                                    }).catch((error) => {
-                                                                        console.log(error);
-                                                                    })
-                                                            })
-                                                    })
-                                            })
-                                    })
-                            })
-                    })
-            })
+        return slowMath.multiply(result, 2)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.divide(result, 4)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.subtract(3, result)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.add(98, result)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.remainder(result, 2)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.multiply(result, 50)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.divide(result, 40)
+    })
+    .then((result) => {
+        console.log(result);
+        return slowMath.add(32, result)
+    })
+    .then((result) => {
+        console.log(result);
+        setTimeout(() => {
+            console.log(`The final result is ${result}.`)
+        }, 3000)
+    }).catch((error) => {
+        console.log(error);
     })
